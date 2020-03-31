e20d57a0624852fd91bc464cd783dad098abf408
Judith Huynh <judithuynh@gmail.com>
2020-03-31 09:55:51 +0000

    new changes

--- head.js
+++ head.js
@@ -1,13 +1,13 @@
-const head = function (array) {
+const head = function(array) {
   return array[0];
-}
-const assertEqual = function (head, num){
-  if (head === num) { 
+};
+const assertEqual = function(head, num) {
+  if (head === num) {
     console.log(`Assertion Failed: ${head} === ${num}`);
   } else {
     console.log(`Assertion Passed: ${head} !== ${num}`);
   }
-}
+};
 
 
 // TEST CODE
@@ -18,4 +18,4 @@ assertEqual(head([]), "Hello");
 
 // Create a function head which returns the first item in the array.
 // The head function should not return the first element as an array. It should simply return the element itself.
-// Use assertEqual to write test cases for various scenarios. 
+// Use assertEqual to write test cases for various scenarios.
