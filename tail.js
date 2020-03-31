1b857d6f88f1d2eaa5f5934444fa1ea09eef104d
Judith Huynh <judithuynh@gmail.com>
2020-03-31 17:38:13 +0000

    new changes with assertEqual

--- tail.js
+++ tail.js
@@ -3,22 +3,21 @@ const tail = function(array) {
   return array.slice(1);
 };
 
-
-
-
-// Write a test case to make sure that the original array is not modified.
-// const assertEqual = function(result){
-// }
-
-
-// const result = tail(["Hello", "Lighthouse", "Labs"]);
-// assertEqual(result.length, 2); // ensure we get back two elements
-// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-
+const assertEqual = function(result, num) {
+  if (result === num) {
+    console.log(`${result} = ${num}`);
+  }
+};
 
 // Test Case: Check the original array
 // const words = ["Yo Yo", "Lighthouse", "Labs"];
+// console.log(words);
 // tail(words); // no need to capture the return value since we are not checking it
+// console.log(tail(words));
 // assertEqual(words.length, 3); // original array should still have 3 elements!
+
+
+// Write a test case to make sure that the original array is not modified.
+// assertEqual(result.length, 2); // ensure we get back two elements
+// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
