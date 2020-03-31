b76b130fa406cb229ddeb80aaa5992913c6267fa
Judith Huynh <judithuynh@gmail.com>
2020-03-31 08:35:55 +0000

    learning assert

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🛑🛑🛑Assertion Failed: " + actual + " === " + expected);
+  } else {
+    console.log("✅✅✅Assertion Passed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcgitamp");
+assertEqual(1, 1);
+
+// console.assert(assertEqual("Lighthouse Labs", "Bootcamp"));
+// console.assert(assertEqual(1, 1));
+
