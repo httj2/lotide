c2451108df64dffe7baa8b338bff6fb10b4d7e3c
Judith Huynh <judithuynh@gmail.com>
2020-04-01 12:09:23 +0000

    takes in two arrays

---
+++ eqArrays.js
@@ -0,0 +1,35 @@
+
+// Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
+const eqArrays = function(arrayOne, arrayTwo) {
+  // compare length
+  if (arrayOne.length !== arrayTwo.length) {
+    console.log(arrayOne.length);
+    console.log(arrayTwo.length);
+    return false;
+  }
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+// eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+// eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
