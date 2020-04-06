c90108b745dc718a95ddefa00b2cb87ea3fd66ae
Judith Huynh <judithuynh@gmail.com>
2020-04-06 17:29:23 +0000

    refactored

--- tail.js
+++ tail.js
@@ -2,22 +2,4 @@
 const tail = function(array) {
   return array.slice(1);
 };
-
-// const assertEqual = function(result, num) {
-//   if (result === num) {
-//     console.log(`${result} = ${num}`);
-//   }
-// };
-
-// Test Case: Check the original array
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
-// console.log(words);
-// tail(words); // no need to capture the return value since we are not checking it
-// console.log(tail(words));
-// assertEqual(words.length, 3); // original array should still have 3 elements!
-
-
-// Write a test case to make sure that the original array is not modified.
-// assertEqual(result.length, 2); // ensure we get back two elements
-// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+module.exports = test;
