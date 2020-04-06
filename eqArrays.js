cf4c7c9e3c082f18336e71c798805250fe04431c
Judith Huynh <judithuynh@gmail.com>
2020-04-06 01:58:31 +0000

    new changes

--- eqArrays.js
+++ eqArrays.js
@@ -1,21 +1,15 @@
 
 // Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
 const eqArrays = function(arrayOne, arrayTwo) {
-  // compare length
   if (arrayOne.length !== arrayTwo.length) {
-    console.log(arrayOne.length);
-    console.log(arrayTwo.length);
-    return false;
-  }
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) {
-      return false;
-    }
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (arrayOne[i] !== arrayTwo[i]) {
+        return false;
+      }
+    } return true
   }
   return true;
 };
-
-
 
 const assertEqual = function(actual, expected) {
   if (actual !== expected) {
@@ -29,7 +23,5 @@ const assertEqual = function(actual, expected) {
 eqArrays([1, 2, 3], [1, 2, 3]); // => true
 eqArrays([1, 2, 3], [3, 2, 1]); // => false
 
-// eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-// eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
