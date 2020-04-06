56c946d719243deb2d1c84fcfaa14a5a69c709b8
Judith Huynh <judithuynh@gmail.com>
2020-04-06 19:55:14 +0000

    exporting functions

---
+++ index.js
@@ -0,0 +1,14 @@
+const assertArraysEqual = require('./assertArraysEqual')
+const assertEqual = require('./assertEqual')
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+
+
+module.exports = {
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  head: head,
+  tail: tail,
+  middle: middle,
+};
