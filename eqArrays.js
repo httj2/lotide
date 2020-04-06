ad896f3c6a4cacec830b2d82b9b5bf735442ce04
Judith Huynh <judithuynh@gmail.com>
2020-04-06 18:17:11 +0000

    refactored

--- eqArrays.js
+++ eqArrays.js
@@ -1,20 +1,17 @@
 
 // Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
-const eqArrays = function(arrayOne, arrayTwo) {
-  if (arrayOne.length === arrayTwo.length) {
-    for (let i = 0; i < arrayOne.length; i++) {
-      if (arrayOne[i] !== arrayTwo[i]) {
-        return false;
-      }
-    }
-  } else {
+const eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
     return false
   }
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i])
+      return false;
+  }
   return true
-};
+}
+
+
 module.exports = eqArrays;
 
-
-// eqArrays([1, 2, 3], [1, 2, 3]); // => true
-// eqArrays([1, 2, 3], [3, 2, 1]); // => false
 
