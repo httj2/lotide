8fe5f8cc0272a48236b38768478cb822b2703464
Judith Huynh <judithuynh@gmail.com>
2020-04-06 19:00:25 +0000

    new changes: mocha and chai refactoring

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,24 @@
 const head = require('../head');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+// const assertEqual = require('../assertEqual');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head([]), "Hello");
+// Refactored
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head([]), "Hello");
+
+// Mocha and Chai based Coding
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5); 
+  });
+  it("returns [] for 'Hello'", () => {
+    assert.isEmpty([]);; 
+  });
+});
