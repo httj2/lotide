8be5321b0b41b5e07eb353764f1e901af86c1d6c
Judith Huynh <judithuynh@gmail.com>
2020-04-06 18:16:47 +0000

    changed

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,7 +1,7 @@
 const eqArrays = require('../eqArrays');
 const assertEqual = require('../assertEqual');
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false);
-assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3, 4]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // pass
+assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), true); // fail
+assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false); // pass
+assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3, 4]), false); // fail
